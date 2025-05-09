58af69eedc1f438ba445a8f860316cac339dffe9
Nguyen Kim An <annk.sale@gmail.com>
2025-05-09 17:12:09 +0700

    Update Fixed CORS, warehouses

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,12 +1,14 @@
 const pingRoutes = require("./ping.routes");
 const authRoutes = require("./auth.routes");
 const userRoutes = require("./user.routes");
+const warehouseRoutes = require('./warehouse.route');
 
 module.exports = (app) => {
   // Register all routes
   app.use("/api/v1/ping", pingRoutes);
   app.use("/api/v1/auth", authRoutes);
   app.use("/api/v1/users", userRoutes);
+  app.use("/api/v1/warehouses", warehouseRoutes);
 
   // Default route for non-existent endpoints
   app.use("*", (req, res) => {
--- server.js
+++ server.js
@@ -1,21 +1,30 @@
-const express = require('express');
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config({ path: ".env.local" });
+
 const app = express();
 const port = process.env.PORT || 3008;
-require('dotenv').config({ path: '.env.local' });
 
+const corsOptions = {
+  origin: "http://localhost:5173", // Thay đổi thành domain của client nếu cần
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true, // Cho phép cookie được gửi
+  optionsSuccessStatus: 204, // Một số trình duyệt cũ (IE11, các trình duyệt cũ) không hỗ trợ 204
+};
 // Middleware
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Routes
-require('./app/routes')(app);
+require("./app/routes")(app);
 
 // Error handling middleware
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({
     success: false,
-    message: err.message || 'Internal Server Error'
+    message: err.message || "Internal Server Error",
   });
 });
 
