b8ea60a16d0fd0a6b458cb5d66e61c0e0a35d2e2
thanhvanba <vanbatrungthanh@gmail.com>
2025-05-12 12:03:06 +0700

    add status for category

--- app/controllers/category.controller.js
+++ app/controllers/category.controller.js
@@ -1,7 +1,7 @@
 const db = require('../config/db.config');
 
 exports.getAllCategories = (req, res, next) => {
-  db.query('SELECT category_id, category_name FROM categories', (err, results) => {
+  db.query('SELECT category_id, category_name, status FROM categories', (err, results) => {
     if (err) return next(err);
     res.json({ success: true, data: results });
   });
@@ -9,33 +9,41 @@ exports.getAllCategories = (req, res, next) => {
 
 exports.getCategoryById = (req, res, next) => {
   const id = req.params.id;
-  db.query('SELECT category_id, category_name FROM categories WHERE category_id = ?', [id], (err, results) => {
-    if (err) return next(err);
-    if (results.length === 0) {
-      return res.status(404).json({ success: false, message: 'Category not found' });
+  db.query(
+    'SELECT category_id, category_name, status FROM categories WHERE category_id = ?',
+    [id],
+    (err, results) => {
+      if (err) return next(err);
+      if (results.length === 0) {
+        return res.status(404).json({ success: false, message: 'Category not found' });
+      }
+      res.json({ success: true, data: results[0] });
     }
-    res.json({ success: true, data: results[0] });
-  });
+  );
 };
 
 exports.createCategory = (req, res, next) => {
-  const { category_name } = req.body;
+  const { category_name, status = 'active' } = req.body;
   db.query(
-    'INSERT INTO categories (category_id, category_name) VALUES (UUID(), ?)',
-    [category_name],
+    'INSERT INTO categories (category_id, category_name, status) VALUES (UUID(), ?, ?)',
+    [category_name, status],
     (err, result) => {
       if (err) return next(err);
-      res.status(201).json({ success: true, message: 'Category created', data: { category_name } });
+      res.status(201).json({
+        success: true,
+        message: 'Category created',
+        data: { category_name, status }
+      });
     }
   );
 };
 
 exports.updateCategory = (req, res, next) => {
   const id = req.params.id;
-  const { category_name } = req.body;
+  const { category_name, status } = req.body;
   db.query(
-    'UPDATE categories SET category_name = ? WHERE category_id = ?',
-    [category_name, id],
+    'UPDATE categories SET category_name = ?, status = ? WHERE category_id = ?',
+    [category_name, status, id],
     (err) => {
       if (err) return next(err);
       res.json({ success: true, message: 'Category updated' });
